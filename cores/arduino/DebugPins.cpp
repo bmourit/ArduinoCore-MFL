08f7878c3e6192f1b53a50c7fce08af22d6b75a9
BMourit <bnmguy@gmail.com>
2025-02-27 13:18:02 -0800

    Fix AFIO set_remap needlessly being called twice

--- cores/arduino/DebugPins.cpp
+++ cores/arduino/DebugPins.cpp
@@ -21,13 +21,11 @@ void freeDebugPins(pin_size_t pin) {
     // JTAG-DP disabled and SW-DP disabled
     if ((gpioPort == gpio::GPIO_Base::GPIOA_BASE) && ((gpioPin == gpio::Pin_Number::PIN_13) || (gpioPin == gpio::Pin_Number::PIN_14))) {
         AFIO_I.set_remap(gpio::Pin_Remap_Select::SWJ_ALL_DISABLED_REMAP);
-        AFIO_I.set_remap(gpio::Pin_Remap_Select::SWJ_ALL_DISABLED_REMAP);
     }
 
     // JTAG-DP disabled and SW-DP enabled
     if (((gpioPort == gpio::GPIO_Base::GPIOA_BASE) && (gpioPin == gpio::Pin_Number::PIN_15)) ||
             ((gpioPort == gpio::GPIO_Base::GPIOB_BASE) && ((gpioPin == gpio::Pin_Number::PIN_3) || (gpioPin == gpio::Pin_Number::PIN_4)))) {
-        AFIO_I.set_remap(gpio::Pin_Remap_Select::SWJ_DP_ONLY_REMAP);
         AFIO_I.set_remap(gpio::Pin_Remap_Select::SWJ_DP_ONLY_REMAP);
     }
 #else
